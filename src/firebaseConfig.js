5e34779a8a3a85337f636854207e3a4e5435e339
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2019-11-30 20:36:21 +0000

    Fixing file issues

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const dbConfig = {
+  apiKey: "AIzaSyBS2OOGldp00eH-h0Adu6GQvzqCxAxwZ60",
+  authDomain: "tumblebot-d43f6.firebaseapp.com",
+  databaseURL: "https://tumblebot-d43f6.firebaseio.com",
+  projectId: "tumblebot-d43f6",
+  storageBucket: "tumblebot-d43f6.appspot.com",
+  messagingSenderId: "796032495913",
+  appId: "1:796032495913:web:c227123cc465c6c2038738",
+  measurementId: "G-FBVEHRMWE4"
+}
