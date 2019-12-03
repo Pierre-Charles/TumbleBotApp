5ca97e39b18a6ed87c4e2d35039282ac529cda8d
Pierre Charles <40791587+Pierre-Charles@users.noreply.github.com>
2019-12-03 12:50:32 +0000

    Changing Bootstrap classes

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -21,7 +21,7 @@ const Chart = props => {
   return (
     <div>
       <h5 className=' p-0 text-center'>Last week's usage</h5>
-      <Bar data={data} wdith={100} height={200} options={
+      <Bar data={data} width={100} height={50} options={
         {
           maintainAspectRatio: true,
           scales: {
